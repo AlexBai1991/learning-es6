9c5a224b117bbb3649b1c243a9391314427bc8c5
ALexBai1991 <baijinguo1991@163.com>
2016-03-12 00:23:11 +0800

    update

--- src/practice/string/basic/index.js
+++ src/practice/string/basic/index.js
@@ -24,3 +24,15 @@ s.includes('Hello', 6) // false
 'x'.repeat(3) // "xxx"
 'hello'.repeat(2) // "hellohello"
 'na'.repeat(0) // ""
+'na'.repeat(2.9) // "nana" 小数会被取整
+
+// padStart padEnd
+'x'.padStart(5, 'ab') // 'ababx'
+'x'.padStart(4, 'ab') // 'abax'
+
+'x'.padEnd(5, 'ab') // 'xabab'
+'x'.padEnd(4, 'ab') // 'xaba'
+
+// 模板字符串
+const whosname = 'Alex Bai <alexbai1991@gmail.com>';
+let modelStr = `hello this is from a guy named ${whosname}`;
